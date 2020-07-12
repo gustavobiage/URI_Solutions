c199c8446ad0f530e74b4c40f584edd87925b5a1
Gustavo de Castro Biage <gustavo.c.biage@gmail.com>
2020-07-11 21:38:43 -0300

    Solution to problem 1034.

--- Paradigms/1034.cpp
+++ Paradigms/1034.cpp
@@ -5,7 +5,6 @@ int T;
 int N, M;
 int arr[30];
 int DP[1000001][30];
-int cnt[1000001][30];
 
 int main() {
 	scanf("%d ", &T);
@@ -14,22 +13,20 @@ int main() {
 		for (int i = 1; i <= N; i++) {
 			scanf("%d ", &arr[i]);
 		}
+		DP[0][0] = 0;
 		for (int i = 1; i <= M; i++) {
+			DP[i][0] = 1000009;
 			for (int j = 1; j <= N; j++) {
 				if (arr[j] > i) {
 					DP[i][j] = DP[i][j-1];
-					cnt[i][j] = cnt[i][j-1];
-				} else if (DP[i-arr[j]][j] + arr[j] > DP[i][j-1] ||
-					DP[i-arr[j]][j] + arr[j] == DP[i][j-1] && cnt[i-arr[j]][j] + 1 < cnt[i][j-1]) {
-					DP[i][j] = DP[i-arr[j]][j] + arr[j];
-					cnt[i][j] = cnt[i-arr[j]][j] + 1;
+				} else if (DP[i-arr[j]][j] + 1 < DP[i][j-1]) {
+					DP[i][j] = DP[i-arr[j]][j] + 1;
 				} else {
 					DP[i][j] = DP[i][j-1];
-					cnt[i][j] = cnt[i][j-1];
 				}
 			}
 		}
-		printf("%d\n", cnt[M][N]);
+		printf("%d\n", DP[M][N]);
 	}
 	return 0;
 }
