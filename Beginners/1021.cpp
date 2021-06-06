ffbfa17080e473e036759f50f324f0509445db9b
Gustavo de Castro Biage <gustavo.c.biage@gmail.com>
2021-06-05 21:23:43 -0300

    Solution to problem 1021.

--- Beginners/1021.cpp
+++ Beginners/1021.cpp
@@ -1,37 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+double notes[] = {100, 50, 20, 10, 5, 2};
+int cnotes = 6;
+double coins[] = {1, 0.50, 0.25, 0.10, 0.05, 0.01};
+int ccoins = 6;
+double value;
+int counter;
+
 int main() {
-	int amnt1, amnt2;
-	int notes[] = {100, 50, 20, 10, 5, 2, 1};
-	int coins[] = {50, 25, 10, 5, 1};
-	int lenNotes = 7;
-	int lenCoins = 5;
+	scanf("%lf ", &value);
+	// Confome mencionado no forum.
+	value += 0.001;
 
-	scanf("%d.%d ", &amnt1, &amnt2);
-	int v = amnt1;
+	int pointer = 0;
 	printf("NOTAS:\n");
-	for (int i = 0; i < lenNotes; i++) {
-		int c = v / notes[i];
-		v = v % notes[i];
-		if (i == lenNotes - 1) {
-			printf("MOEDAS:\n");
-			printf("%d moeda(s) de R$ 1.00\n", c);
-		} else {
-			printf("%d nota(s) de R$ %d.00\n", c, notes[i]);
-		}
+	while (pointer < cnotes) {
+		counter = 0;
+		counter = (int) (value / notes[pointer]);
+		value = value - counter * notes[pointer];
+		printf("%d nota(s) de R$ %0.2lf\n", counter, notes[pointer]);
+		pointer++;
 	}
-
-	v = amnt2;
-	for (int i = 0; i < lenCoins; i++) {
-		int c = v / coins[i];
-		v = v % coins[i];
-		if (i >= lenCoins - 2) {
-			printf("%d moeda(s) de R$ 0.0%d\n", c, coins[i]);
-		} else {
-			printf("%d moeda(s) de R$ 0.%d\n", c, coins[i]);
-		}
+	pointer = 0;
+	printf("MOEDAS:\n");
+	while (pointer < ccoins) {
+		counter = (int) (value / coins[pointer]);
+		value = value - counter * coins[pointer];
+		printf("%d moeda(s) de R$ %0.2lf\n", counter, coins[pointer]);
+		pointer++;
 	}
-
 	return 0;
 }
